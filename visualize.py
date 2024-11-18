5cdb8b23ef27a325075541608adc27b0610008b3
HAI NAM LE <s222576762@gandalf-dev.it.deakin.edu.au>
2024-11-18 12:42:26 +1100

    Update IBD-PSC.py and visualize.py

--- visualize.py
+++ visualize.py
@@ -7,19 +7,20 @@ import argparse
 from torch import nn
 from utils import supervisor, tools, default_args
 import config
-from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.manifold import TSNE, Isomap, LocallyLinearEmbedding, SpectralEmbedding
 from matplotlib import pyplot as plt
 from sklearn import svm
-from sklearn.metrics import silhouette_score
+from umap import UMAP
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-method', type=str, required=False, default='pca',
-                    choices=['pca', 'tsne', 'oracle', 'mean_diff', 'SS'])
+                    choices=['pca', 'tsne', 'umap', 'oracle', 'mean_diff', 'SS',
+                             'isomap', 'lle', 'kpca', 'spectral'])
 parser.add_argument('-dataset', type=str, required=False, default=default_args.parser_default['dataset'],
                     choices=default_args.parser_choices['dataset'])
 parser.add_argument('-poison_type', type=str,  required=True,
-        choices=default_args.parser_choices['poison_type'])
+                    choices=default_args.parser_choices['poison_type'])
 parser.add_argument('-poison_rate', type=float,  required=False,
                     choices=default_args.parser_choices['poison_rate'],
                     default=default_args.parser_default['poison_rate'])
@@ -82,15 +83,8 @@ class oracle_visualizer:
         poison = poison.numpy()
         num_poison = len(poison)
 
-        # print(clean.shape, poison.shape)
-
         X = np.concatenate([clean, poison], axis=0)
-        y = []
-
-        for _ in range(num_clean):
-            y.append(0)
-        for _ in range(num_poison):
-            y.append(1)
+        y = [0] * num_clean + [1] * num_poison
 
         self.clf.fit(X, y)
         print("SVM Accuracy:", self.clf.score(X, y))
@@ -110,7 +104,7 @@ class spectral_visualizer:
         all_features = torch.cat((clean, poison), dim=0)
         all_features -= all_features.mean(dim=0)
         _, _, V = torch.svd(all_features, compute_uv=True, some=False)
-        vec = V[:, 0]  # the top right singular vector is the first column of V
+        vec = V[:, 0]  # vector kỳ dị chính
         vals = []
         for j in range(all_features.shape[0]):
             vals.append(torch.dot(all_features[j], vec).pow(2))
@@ -119,7 +113,6 @@ class spectral_visualizer:
         print(vals.shape)
 
         return vals[:clean.shape[0]], vals[clean.shape[0]:]
-
 
 
 if args.dataset == 'cifar10':
@@ -133,26 +126,24 @@ else:
 
 data_transform_aug, data_transform, trigger_transform, normalizer, denormalizer = supervisor.get_transforms(args)
 
-
 arch = supervisor.get_arch(args)
-# Set up Poisoned Set
+# Thiết lập tập dữ liệu nhiễm độc
 poison_set_dir = supervisor.get_poison_set_dir(args)
-if os.path.exists(os.path.join(poison_set_dir, 'data')): # if old version
+if os.path.exists(os.path.join(poison_set_dir, 'data')):  # phiên bản cũ
     poisoned_set_img_dir = os.path.join(poison_set_dir, 'data')
-if os.path.exists(os.path.join(poison_set_dir, 'imgs')): # if new version
+if os.path.exists(os.path.join(poison_set_dir, 'imgs')):  # phiên bản mới
     poisoned_set_img_dir = os.path.join(poison_set_dir, 'imgs')
 poisoned_set_label_path = os.path.join(poison_set_dir, 'labels')
 poison_indices_path = os.path.join(poison_set_dir, 'poison_indices')
 
 poisoned_set = tools.IMG_Dataset(data_dir=poisoned_set_img_dir,
-                                    label_path=poisoned_set_label_path, transforms=data_transform)
+                                 label_path=poisoned_set_label_path, transforms=data_transform)
 
 poisoned_set_loader = torch.utils.data.DataLoader(
-        poisoned_set,
-        batch_size=batch_size, shuffle=False, **kwargs)
+    poisoned_set,
+    batch_size=batch_size, shuffle=False, **kwargs)
 
 poison_indices = torch.tensor(torch.load(poison_indices_path))
-
 
 test_set_dir = 'clean_set/%s/test_split/' % args.dataset
 test_set_img_dir = os.path.join(test_set_dir, 'data')
@@ -164,23 +155,8 @@ test_set_loader = torch.utils.data.DataLoader(
     batch_size=batch_size, shuffle=False, **kwargs
 )
 
-
-
-
-
 model_list = []
 alias_list = []
-
-
-"""
-if args.poison_type == 'none':  # no poison => load vanilla data and model
-    path = os.path.join('models', '%s_vanilla_no_aug.pt' % args.dataset)
-    model_list.append(path)
-    alias_list.append('vanilla_no_aug')
-
-    path = os.path.join('models', '%s_vanilla_aug.pt' % args.dataset)
-    model_list.append(path)
-    alias_list.append('vanilla_aug')"""
 
 if (hasattr(args, 'model_path') and args.model_path is not None) or (hasattr(args, 'model') and args.model is not None):
     path = supervisor.get_model_dir(args)
@@ -188,62 +164,49 @@ if (hasattr(args, 'model_path') and args.model_path is not None) or (hasattr(arg
     alias_list.append('assigned')
 
 else:
-    # args.no_aug = True
-    # #path = os.path.join(poison_set_dir, 'full_base_no_aug.pt') #
-    # path = supervisor.get_model_dir(args)
-    # model_list.append(path)
-    # alias_list.append(supervisor.get_model_name(args))
-
     args.no_aug = False
-    #path = os.path.join(poison_set_dir, 'full_base_aug.pt') #supervisor.get_model_dir(args)
     path = supervisor.get_model_dir(args)
     model_list.append(path)
     alias_list.append(supervisor.get_model_name(args))
 
-
 poison_transform = supervisor.get_poison_transform(poison_type=args.poison_type, dataset_name=args.dataset,
-                                                       target_class=target_class,
-                                                       trigger_transform=data_transform,
-                                                       is_normalized_input=True,
-                                                       alpha=args.alpha if args.test_alpha is None else args.test_alpha,
-                                                       trigger_name=args.trigger, args=args)
-
+                                                   target_class=target_class,
+                                                   trigger_transform=data_transform,
+                                                   is_normalized_input=True,
+                                                   alpha=args.alpha if args.test_alpha is None else args.test_alpha,
+                                                   trigger_name=args.trigger, args=args)
 
 if args.poison_type == 'TaCT':
     source_classes = [config.source_class]
 else:
     source_classes = None
 
-
 for vid, path in enumerate(model_list):
 
     ckpt = torch.load(path)
 
-    # base model for poison detection
+    # mô hình cơ sở để phát hiện poison
     model = arch(num_classes=num_classes)
     model.load_state_dict(ckpt)
     model = nn.DataParallel(model)
     model = model.cuda()
     model.eval()
 
-
-    # Begin Visualization
+    # Bắt đầu trực quan hóa
     print("Visualizing model '{}' on {}...".format(path, args.dataset))
 
     print('[test]')
-    tools.test(model, test_set_loader, poison_test=True, poison_transform=poison_transform, num_classes=num_classes, source_classes=source_classes)
-
-
+    tools.test(model, test_set_loader, poison_test=True, poison_transform=poison_transform, num_classes=num_classes,
+               source_classes=source_classes)
 
     targets = []
     features = []
     clean_features = []
     poisoned_features = []
 
-
     with torch.no_grad():
         for batch_idx, (data, target) in enumerate(poisoned_set_loader):
-            data, target = data.cuda(), target.cuda()  # train set batch
+            data, target = data.cuda(), target.cuda()  # batch của tập train
             targets.append(target)
             _, feature = model.forward(data, return_hidden=True)
             features.append(feature.cpu().detach())
@@ -259,11 +222,20 @@ for vid, path in enumerate(model_list):
             visualizer = PCA(n_components=2)
         elif args.method == 'tsne':
             visualizer = TSNE(n_components=2)
+        elif args.method == 'umap':
+            visualizer = UMAP(n_components=2)
+        elif args.method == 'isomap':
+            visualizer = Isomap(n_components=2)
+        elif args.method == 'lle':
+            visualizer = LocallyLinearEmbedding(n_components=2)
+        elif args.method == 'kpca':
+            visualizer = KernelPCA(n_components=2, kernel='rbf')
+        elif args.method == 'spectral':
+            visualizer = SpectralEmbedding(n_components=2)
         else:
             raise NotImplementedError('Visualization Method %s is Not Implemented!' % args.method)
 
         non_poison_indices = list(set(list(range(len(poisoned_set)))) - set(poison_indices.tolist()))
-        #print(non_poison_indices)
         clean_targets = targets[non_poison_indices]
         print("Total Clean:", len(clean_targets))
         print("Total Poisoned:", 0)
@@ -274,21 +246,19 @@ for vid, path in enumerate(model_list):
         clean_ids = ids[non_poison_indices]
         class_clean_ids = clean_ids[clean_targets == target_class]
 
-        reduced_features = visualizer.fit_transform(
-            class_clean_features)  # all features vector under the label i
+        feats_np = class_clean_features.numpy()
 
-        #plt.scatter(reduced_features[:, 0], reduced_features[:, 1], facecolors='none', marker='o',
-        #            color='blue', label='clean')
+        reduced_features = visualizer.fit_transform(feats_np)  # tất cả vector đặc trưng dưới nhãn i
 
         plt.scatter(reduced_features[:, 0], reduced_features[:, 1], marker='o', color='blue', s=5, alpha=0.5)
         plt.axis('off')
-        save_path = 'assets/%s_%s_%s_class=%d.png' % (args.method, supervisor.get_dir_core(args, include_poison_seed=True), alias_list[vid], target_class)
+        save_path = 'assets/%s_%s_%s_class=%d.png' % (
+            args.method, supervisor.get_dir_core(args, include_poison_seed=True), alias_list[vid], target_class)
         plt.savefig(save_path)
         print("Saved figure at {}".format(save_path))
         plt.clf()
 
     else:
-
 
         non_poison_indices = list(set(list(range(len(poisoned_set)))) - set(poison_indices.tolist()))
 
@@ -313,12 +283,10 @@ for vid, path in enumerate(model_list):
         num_poisoned = len(class_poisoned_features)
 
         feats = torch.cat([class_clean_features, class_poisoned_features], dim=0)
-        ids = list(range(0,len(feats)))
-        random.shuffle(ids)
-        #class_clean_features = feats[ids[:num_clean]]
-        #class_poisoned_features = feats[ids[-num_poisoned:]]
-        # class_poisoned_features = poisoned_features
+        labels = [0] * num_clean + [1] * num_poisoned
 
+        feats_np = feats.cpu().numpy()
+        labels_np = np.array(labels)
 
         class_clean_mean = class_clean_features.mean(dim=0)
         print(class_clean_mean.shape)
@@ -326,15 +294,22 @@ for vid, path in enumerate(model_list):
         poison_dis = torch.norm(class_poisoned_features - class_clean_mean, dim=1).mean()
         print('clean_dis: %f, poison_dis: %f' % (clean_dis, poison_dis))
 
-        tmp_labels = [0] * len(class_clean_features) + [1] * len(class_poisoned_features)
-        silhouette = silhouette_score(feats, tmp_labels)
-        print('Silhouette Score:', silhouette)
-        # exit()
+        tmp_labels = [0] * num_clean + [1] * num_poisoned
 
         if args.method == 'pca':
             visualizer = PCA(n_components=2)
         elif args.method == 'tsne':
             visualizer = TSNE(n_components=2)
+        elif args.method == 'umap':
+            visualizer = UMAP(n_components=2)
+        elif args.method == 'isomap':
+            visualizer = Isomap(n_components=2)
+        elif args.method == 'lle':
+            visualizer = LocallyLinearEmbedding(n_components=2)
+        elif args.method == 'kpca':
+            visualizer = KernelPCA(n_components=2, kernel='rbf')
+        elif args.method == 'spectral':
+            visualizer = SpectralEmbedding(n_components=2)
         elif args.method == 'oracle':
             visualizer = oracle_visualizer()
         elif args.method == 'mean_diff':
@@ -344,69 +319,57 @@ for vid, path in enumerate(model_list):
         else:
             raise NotImplementedError('Visualization Method %s is Not Implemented!' % args.method)
 
-
         if args.method == 'oracle':
             clean_projection, poison_projection = visualizer.fit_transform(class_clean_features,
                                                                            class_poisoned_features)
-            # print(clean_projection)
-            # print(poison_projection)
-
-            # bins = np.linspace(-2, 2, 100)
             plt.figure(figsize=(7, 5))
-            # plt.xlim([-3, 3])
             plt.ylim([0, 100])
 
             plt.hist(clean_projection, bins='doane', color='blue', alpha=0.5, label='Clean', edgecolor='black')
             plt.hist(poison_projection, bins='doane', color='red', alpha=0.5, label='Poison', edgecolor='black')
-            
-            # plt.xlabel("Distance")
-            # plt.ylabel("Number")
-            # plt.axis('off')
-            # plt.legend()
+            plt.xlabel("Projection")
+            plt.ylabel("Number")
+            plt.legend()
+
         elif args.method == 'mean_diff':
             clean_projection, poison_projection = visualizer.fit_transform(class_clean_features, class_poisoned_features)
-            # all_projection = torch.cat((clean_projection, poison_projection), dim=0)
-
-            # bins = np.linspace(-5, 5, 50)
             plt.figure(figsize=(7, 5))
-
-            # plt.hist(all_projection.cpu().detach().numpy(), bins='doane', alpha=1, label='all', linestyle='dashed', color='black', histtype="step", edgecolor='black')
-            plt.hist(clean_projection.cpu().detach().numpy(), color='blue', bins='doane', alpha=0.5, label='Clean', edgecolor='black')
-            plt.hist(poison_projection.cpu().detach().numpy(), color='red', bins='doane', alpha=0.5, label='Poison', edgecolor='black')
-            
-            plt.xlabel("Distance")
+            plt.hist(clean_projection.cpu().detach().numpy(), color='blue', bins='doane', alpha=0.5, label='Clean',
+                     edgecolor='black')
+            plt.hist(poison_projection.cpu().detach().numpy(), color='red', bins='doane', alpha=0.5, label='Poison',
+                     edgecolor='black')
+            plt.xlabel("Projection")
             plt.ylabel("Number")
             plt.legend()
+
         elif args.method == 'SS':
             clean_projection, poison_projection = visualizer.fit_transform(class_clean_features, class_poisoned_features)
-            # all_projection = torch.cat((clean_projection, poison_projection), dim=0)
-
-            # bins = np.linspace(-5, 5, 50)
             plt.figure(figsize=(7, 5))
             plt.ylim([0, 300])
-
-            # plt.hist(all_projection.cpu().detach().numpy(), bins='doane', alpha=1, label='all', linestyle='dashed', color='black', histtype="step", edgecolor='black')
-            plt.hist(clean_projection.cpu().detach().numpy(), color='blue', bins='doane', alpha=0.5, label='Clean', edgecolor='black')
-            plt.hist(poison_projection.cpu().detach().numpy(), color='red', bins=20, alpha=0.5, label='Poison', edgecolor='black')
-            
-            plt.xlabel("Distance")
+            plt.hist(clean_projection.cpu().detach().numpy(), color='blue', bins='doane', alpha=0.5, label='Clean',
+                     edgecolor='black')
+            plt.hist(poison_projection.cpu().detach().numpy(), color='red', bins=20, alpha=0.5, label='Poison',
+                     edgecolor='black')
+            plt.xlabel("Projection")
             plt.ylabel("Number")
             plt.legend()
-        else:
-            reduced_features = visualizer.fit_transform( torch.cat([class_clean_features, class_poisoned_features], dim=0) ) # all features vector under the label
 
+        elif args.method in ['pca', 'tsne', 'umap', 'isomap', 'lle', 'kpca', 'spectral']:
+            reduced_features = visualizer.fit_transform(feats_np)
             plt.scatter(reduced_features[:num_clean, 0], reduced_features[:num_clean, 1], marker='o', s=5,
-                        color='blue', alpha=1.0)
+                        color='blue', alpha=1.0, label='Clean')
             plt.scatter(reduced_features[num_clean:, 0], reduced_features[num_clean:, 1], marker='^', s=8,
-                        color='red', alpha=0.7)
-
-
+                        color='red', alpha=0.7, label='Poisoned')
             plt.axis('off')
+            plt.legend()
 
+        else:
+            raise NotImplementedError('Visualization Method %s is Not Implemented!' % args.method)
 
-        save_path = 'assets/%s_%s_%s_class=%d.png' % (args.method, supervisor.get_dir_core(args, include_poison_seed=True), alias_list[vid], target_class)
+        save_path = 'assets/%s_%s_%s_class=%d.png' % (
+            args.method, supervisor.get_dir_core(args, include_poison_seed=True), alias_list[vid], target_class)
         plt.tight_layout()
         plt.savefig(save_path)
         print("Saved figure at {}".format(save_path))
-
         plt.clf()
+
