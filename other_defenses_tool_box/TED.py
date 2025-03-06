ae4d4b0ddf9d93c4ab1768c771fbf43fd8ba9f46
tomledeakin <tomledeakin@gmai.com>
2025-03-07 00:39:56 +1100

    update

--- other_defenses_tool_box/TED.py
+++ other_defenses_tool_box/TED.py
@@ -668,11 +668,11 @@ class TED(BackdoorDefense):
                         """
                         mask = ~torch.all(processing_label_h_defense_activation == h_defense_activation[idx], dim=1)
                         sorted_dis_validation, sorted_indices_validation = self.get_dis_sort(h_defense_activation[idx], processing_label_h_defense_activation[mask])
-                        threshold = torch.max(sorted_dis_validation[:4])
+                        threshold = torch.max(sorted_dis_validation[:3])
                         distance_value = sorted_dis[i].item()
 
                         if distance_value > threshold:
-                            distance_value_index = 999999
+                            distance_value_index = self.DEFENSE_TRAIN_SIZE - 1
                         else:
                             distance_value_index = i - 1
 
@@ -709,11 +709,11 @@ class TED(BackdoorDefense):
                     if h_defense_prediction[idx] == processing_label:
                         mask = ~torch.all(processing_label_h_defense_activation == h_defense_activation[idx], dim=1)
                         sorted_dis_validation, sorted_indices_validation = self.get_dis_sort(h_defense_activation[idx], processing_label_h_defense_activation[mask])
-                        threshold = torch.max(sorted_dis_validation[:4])
+                        threshold = torch.max(sorted_dis_validation[:3])
                         distance_value = sorted_dis[i].item()
 
                         if distance_value > threshold:
-                            distance_value_index = 999999
+                            distance_value_index = self.DEFENSE_TRAIN_SIZE - 1
                         else:
                             distance_value_index = i
 
