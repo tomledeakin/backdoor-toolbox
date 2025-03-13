516626b3155056d1051c68c3b178fd4096b8e3ef
tomledeakin <tomledeakin@gmai.com>
2025-03-13 13:15:59 +1100

    update

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 from datasets import load_dataset
 
-dataset = load_dataset('parquet', data_files={
-    'train': 'data/tiny-imagenet-200-clean/data/train-00000-of-00001.parquet',
-    'validation': 'data/tiny-imagenet-200-clean/data/validation-00000-of-00001.parquet',
-    'test': 'data/tiny-imagenet-200-clean/data/test-00000-of-00001.parquet',
-})
+dataset = load_dataset('slegroux/tiny-imagenet-200-clean')
 
 print(dataset)
