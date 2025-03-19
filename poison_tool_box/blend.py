66fce07f734e4a9d37850c2d37a3e13608f1bee5
tomledeakin <tomledeakin@gmai.com>
2025-03-19 18:24:44 +1100

    update

--- poison_tool_box/blend.py
+++ poison_tool_box/blend.py
@@ -3,75 +3,79 @@ import torch
 import random
 from torchvision.utils import save_image
 
+
 class poison_generator():
-
-    def __init__(self, img_size, dataset, poison_rate, trigger, path, target_class = 0, alpha = 0.2):
-
+    def __init__(self, img_size, dataset, poison_rate, trigger, path, target_class=0, alpha=0.2):
         self.img_size = img_size
         self.dataset = dataset
         self.poison_rate = poison_rate
-        self.trigger = trigger
+        self.trigger = trigger  # trigger được truyền vào (tensor)
         self.path = path  # path to save the dataset
-        self.target_class = target_class # by default : target_class = 0
+        self.target_class = target_class  # by default: target_class = 0
         self.alpha = alpha
 
         # number of images
         self.num_img = len(dataset)
 
     def generate_poisoned_training_set(self):
-
         # random sampling
-        id_set = list(range(0,self.num_img))
+        id_set = list(range(self.num_img))
         random.shuffle(id_set)
         num_poison = int(self.num_img * self.poison_rate)
-        poison_indices = id_set[:num_poison]
-        poison_indices.sort() # increasing order
+        poison_indices = sorted(id_set[:num_poison])
+
 
         img_set = []
         label_set = []
         pt = 0
         for i in range(self.num_img):
             img, gt = self.dataset[i]
+            # In debug, in ra shape của ảnh trước khi áp dụng trigger
+            # print(f"[DEBUG] Image {i} shape before: {img.shape}")
 
             if pt < num_poison and poison_indices[pt] == i:
                 gt = self.target_class
-                img = (1 - self.alpha) * img + self.alpha *  self.trigger
-                pt+=1
+                # Nếu số kênh của ảnh và trigger không khớp, điều chỉnh trigger về dạng grayscale
+                if img.shape[0] != self.trigger.shape[0]:
+                    adjusted_trigger = self.trigger.mean(dim=0, keepdim=True)
 
-            # img_file_name = '%d.png' % i
-            # img_file_path = os.path.join(self.path, img_file_name)
-            # save_image(img, img_file_path)
-            #print('[Generate Poisoned Set] Save %s' % img_file_path)
-            
+                else:
+                    adjusted_trigger = self.trigger
+                # Blend ảnh với trigger
+                img = (1 - self.alpha) * img + self.alpha * adjusted_trigger
+                pt += 1
+
             img_set.append(img.unsqueeze(0))
             label_set.append(gt)
 
         img_set = torch.cat(img_set, dim=0)
         label_set = torch.LongTensor(label_set)
 
+
         return img_set, poison_indices, label_set
 
 
-
 class poison_transform():
-    def __init__(self, img_size, trigger, target_class = 0, alpha = 0.2):
+    def __init__(self, img_size, trigger, target_class=0, alpha=0.2):
         self.img_size = img_size
-        self.trigger = trigger
-        self.target_class = target_class # by default : target_class = 0
+        self.trigger = trigger  # trigger tensor
+        self.target_class = target_class  # by default: target_class = 0
         self.alpha = alpha
-        
+
     def transform(self, data, labels):
         data = data.clone()
         labels = labels.clone()
-        # transform clean samples to poison samples
+
+        # Nếu số kênh của data không khớp với trigger, điều chỉnh trigger về dạng grayscale
+        if data.shape[1] != self.trigger.shape[0]:
+            adjusted_trigger = self.trigger.mean(dim=0, keepdim=True)
+
+        else:
+            adjusted_trigger = self.trigger
+
+        # Áp dụng blend: giữ lại phần (1-alpha)*data và trộn alpha*trigger
+        data = (1 - self.alpha) * data + self.alpha * adjusted_trigger.to(data.device)
         labels[:] = self.target_class
-        data = (1 - self.alpha) * data + self.alpha * self.trigger.to(data.device)
-        
-        # debug
-        # from torchvision.utils import save_image
-        # from torchvision import transforms
-        # preprocess = transforms.Normalize([0.4914, 0.4822, 0.4465], [0.247, 0.243, 0.261])
-        # reverse_preprocess = transforms.Normalize([-0.4914/0.247, -0.4822/0.243, -0.4465/0.261], [1/0.247, 1/0.243, 1/0.261])
-        # save_image(reverse_preprocess(data)[-7], 'a.png')
+
 
         return data, labels
