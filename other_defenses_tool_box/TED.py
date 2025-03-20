7ce69f5b9538d25c1860079a5b526c731a591459
tomledeakin <tomledeakin@gmai.com>
2025-03-21 03:34:44 +1100

    update

--- other_defenses_tool_box/TED.py
+++ other_defenses_tool_box/TED.py
@@ -83,7 +83,7 @@ class TED(BackdoorDefense):
             self.testset = self.test_loader.dataset
 
         print(f"Number of samples in full test set: {len(self.testset)}")
-        self.SELECTED_CLASSES = 40
+        self.SELECTED_CLASSES = 20
 
         ########################################################################
         # BỔ SUNG: CHỈ LỰA CHỌN NGẪU NHIÊN 30 CLASSES (BAO GỒM TARGET CLASS)
@@ -166,7 +166,10 @@ class TED(BackdoorDefense):
 
         # 6) Set defense training parameters
         self.SAMPLES_PER_CLASS = args.validation_per_class
-        self.DEFENSE_TRAIN_SIZE = self.num_classes * self.SAMPLES_PER_CLASS
+        if self.dataset == "tinyimagenet200":
+            self.DEFENSE_TRAIN_SIZE = self.SELECTED_CLASSES * self.SAMPLES_PER_CLASS
+        else:
+            self.DEFENSE_TRAIN_SIZE = self.num_classes * self.SAMPLES_PER_CLASS
 
         # 7) Define number of neighbors and samples for constructing poison/clean sets
         self.NUM_SAMPLES = args.num_test_samples
