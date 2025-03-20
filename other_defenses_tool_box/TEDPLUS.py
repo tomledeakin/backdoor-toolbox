ceee4143ea46bb3a08f88f54958f0679b6cadcd9
tomledeakin <tomledeakin@gmai.com>
2025-03-21 04:08:11 +1100

    update

--- other_defenses_tool_box/TEDPLUS.py
+++ other_defenses_tool_box/TEDPLUS.py
@@ -83,7 +83,7 @@ class TEDPLUS(BackdoorDefense):
             self.testset = self.test_loader.dataset
 
         print(f"Number of samples in full test set: {len(self.testset)}")
-        self.SELECTED_CLASSES = 20
+        self.SELECTED_CLASSES = 40
 
         ########################################################################
         # BỔ SUNG: CHỈ LỰA CHỌN NGẪU NHIÊN 30 CLASSES (BAO GỒM TARGET CLASS)
