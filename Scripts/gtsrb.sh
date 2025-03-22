eb3f0045ae7c77dd91eee25ce1ad5599f1df867e
tomledeakin <tomledeakin@gmai.com>
2025-03-23 00:50:32 +1100

    update

--- Scripts/gtsrb.sh
+++ Scripts/gtsrb.sh
@@ -21,11 +21,9 @@ export PYTHONUNBUFFERED=1
 # python train_SSDT.py --dataset cifar10 --attack_mode SSDT --n_iters 200
 # python train_SSDT.py --dataset gtsrb --attack_mode SSDT --n_iters 200
 
-python create_poisoned_set.py -dataset=cifar10 -poison_type=clean_label -poison_rate=0.1
-python train_on_poisoned_set.py -dataset=cifar10 -poison_type=clean_label -poison_rate=0.1
+python create_poisoned_set.py -dataset=cifar10 -poison_type=clean_label -poison_rate=0.01
+python train_on_poisoned_set.py -dataset=cifar10 -poison_type=clean_label -poison_rate=0.01
 
-python create_poisoned_set.py -dataset=gtsrb -poison_type=clean_label -poison_rate=0.1
-python train_on_poisoned_set.py -dataset=gtsrb -poison_type=clean_label -poison_rate=0.1
 
 # echo "cifar10 - badnet | START"
 # # python create_poisoned_set.py -dataset=cifar10 -poison_type=badnet -poison_rate='0.1'
