d88cbd2c09f61897e795a0db1c76911717b5b237
tomledeakin <tomledeakin@gmai.com>
2025-03-26 23:04:06 +1100

    update

--- Scripts/gtsrb.sh
+++ Scripts/gtsrb.sh
@@ -17,10 +17,13 @@ source "my_env/bin/activate"
 cd "$HOME/BackdoorBox Research/backdoor-toolbox"
 export PYTHONUNBUFFERED=1
 
-python other_defense.py -defense=TED -dataset=tinyimagenet200 -poison_type=badnet -poison_rate=0.1 -validation_per_class=10 -num_test_samples=50
-python other_defense.py -defense=TEDPLUS -dataset=tinyimagenet200 -poison_type=badnet -poison_rate=0.1 -validation_per_class=10 -num_test_samples=50
-python other_defense.py -defense=TED -dataset=tinyimagenet200 -poison_type=blend -poison_rate=0.1 -validation_per_class=10 -num_test_samples=50
-python other_defense.py -defense=TEDPLUS -dataset=tinyimagenet200 -poison_type=blend -poison_rate=0.1 -validation_per_class=10 -num_test_samples=50
+python create_poisoned_set.py -dataset=tinyimagenet200 -poison_type=trojan -poison_rate=0.1
+python train_on_poisoned_set.py -dataset=tinyimagenet200 -poison_type=trojan -poison_rate=0.1 -resume_from_meta_info
+
+#python other_defense.py -defense=TED -dataset=tinyimagenet200 -poison_type=badnet -poison_rate=0.1 -validation_per_class=10 -num_test_samples=50
+#python other_defense.py -defense=TEDPLUS -dataset=tinyimagenet200 -poison_type=badnet -poison_rate=0.1 -validation_per_class=10 -num_test_samples=50
+#python other_defense.py -defense=TED -dataset=tinyimagenet200 -poison_type=blend -poison_rate=0.1 -validation_per_class=10 -num_test_samples=50
+#python other_defense.py -defense=TEDPLUS -dataset=tinyimagenet200 -poison_type=blend -poison_rate=0.1 -validation_per_class=10 -num_test_samples=50
 
 #python create_poisoned_set.py -dataset=tinyimagenet200 -poison_type=blend -poison_rate=0.1
 #python train_on_poisoned_set.py -dataset=tinyimagenet200 -poison_type=blend -poison_rate=0.1 -resume_from_meta_info
